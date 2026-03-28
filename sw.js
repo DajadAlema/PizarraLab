e81322598cdb1f014c2eadf664e31408adb16b1e
DajadAlema <90935096+DajadAlema@users.noreply.github.com>
2026-03-28 16:46:21 -0600

    Update CACHE_NAME for service worker

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  * 2. Manejar notificaciones push (cuando la app está cerrada)
  */
 
-const CACHE_NAME = 'pizarralab-v2-calendaradd';
+const CACHE_NAME = 'pizarralab-v2.0.0.1-calendaradd';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
