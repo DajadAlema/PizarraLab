1f93e26d644fa775adf6725839741c062a7cd755
DajadAlema <90935096+DajadAlema@users.noreply.github.com>
2026-04-14 14:45:13 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  * 2. Manejar notificaciones push (cuando la app está cerrada)
  */
 
-const CACHE_NAME = 'pizarralab-v2.6.7.3-HeaderReorganization';
+const CACHE_NAME = 'pizarralab-v2.6.7.4-HeaderReorganization';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
